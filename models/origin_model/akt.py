1239f79c8561190e3308d7aa281bee536698093c
Wei Tingjiang <mxdlzg@163.com>
2025-01-08 09:09:07 +0800

    添加c_qdkt. 添加xes3g5m数据集

--- models/origin_model/akt.py
+++ models/origin_model/akt.py
@@ -3,8 +3,8 @@ from wandb_train import main
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset_name", type=str, default="nips_task34")
-    parser.add_argument("--model_name", type=str, default="cakt")
+    parser.add_argument("--dataset_name", type=str, default="xes3g5m")
+    parser.add_argument("--model_name", type=str, default="akt")
     parser.add_argument("--emb_type", type=str, default="qid")
     parser.add_argument("--save_dir", type=str, default="saved_model")
     # parser.add_argument("--learning_rate", type=float, default=1e-5)
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     parser.add_argument("--n_blocks", type=int, default=4)
     parser.add_argument("--learning_rate", type=float, default=1e-4)
 
-    parser.add_argument("--use_wandb", type=int, default=0)
+    parser.add_argument("--use_wandb", type=int, default=1)
     parser.add_argument("--add_uuid", type=int, default=1)
 
     args = parser.parse_args()

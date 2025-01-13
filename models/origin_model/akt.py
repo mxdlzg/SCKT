57944ca0bcfc9a657f4f6b2b06199474571e9555
Wei Tingjiang <mxdlzg@163.com>
2025-01-13 12:46:03 +0800

    改造cakt，适配module test

--- models/origin_model/akt.py
+++ models/origin_model/akt.py
@@ -3,10 +3,10 @@ from wandb_train import main
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset_name", type=str, default="xes3g5m")
-    parser.add_argument("--model_name", type=str, default="akt")
+    parser.add_argument("--dataset_name", type=str, default="nips_task34")
+    parser.add_argument("--model_name", type=str, default="cakt")
     parser.add_argument("--emb_type", type=str, default="qid")
-    parser.add_argument("--save_dir", type=str, default="saved_model")
+    parser.add_argument("--save_dir", type=str, default="saved_model_module_test")
     # parser.add_argument("--learning_rate", type=float, default=1e-5)
     parser.add_argument("--seed", type=int, default=3407)
     parser.add_argument("--fold", type=int, default=0)
@@ -18,10 +18,20 @@ if __name__ == "__main__":
     parser.add_argument("--n_blocks", type=int, default=4)
     parser.add_argument("--learning_rate", type=float, default=1e-4)
 
-    parser.add_argument("--use_wandb", type=int, default=1)
+    parser.add_argument("--use_time", action="store_true", default=False)
+    parser.add_argument("--use_time_q", action="store_true", default=False)
+    parser.add_argument("--use_time_pid", action="store_true", default=False)
+
+    parser.add_argument("--use_wandb", type=int, default=0)
     parser.add_argument("--add_uuid", type=int, default=1)
 
     args = parser.parse_args()
+
+    # if model_name is cstabelkt, add
+    if args.model_name not in ["cakt"]:
+        del args.use_time
+        del args.use_time_q
+        del args.use_time_pid
 
     params = vars(args)
     main(params)
